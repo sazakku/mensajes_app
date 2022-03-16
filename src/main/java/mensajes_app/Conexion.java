6e7802b2c869001f19fd444fceb842ec32106f6a
Andres Galeano <afgaleano91@misena.edu.co>
2022-03-15 20:02:39 -0500

    Primer commit, configuración de app

---
+++ src/main/java/mensajes_app/Conexion.java
@@ -0,0 +1,23 @@
+package mensajes_app;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    public Connection get_connection(){
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_persistence", "root", "");
+
+            if (connection != null) {
+                System.out.println("Conexión exitosa");
+            }
+        } catch (SQLException e ){
+            System.out.println(e);
+        }
+
+        return connection;
+    }
+}
