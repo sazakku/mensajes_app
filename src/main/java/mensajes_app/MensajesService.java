51c655e251d472267f8bc1da50a789def4feedb7
Andres Galeano <afgaleano91@misena.edu.co>
2022-03-15 20:56:59 -0500

    Create layer for connection to DB

---
+++ src/main/java/mensajes_app/MensajesService.java
@@ -0,0 +1,20 @@
+package mensajes_app;
+
+public class MensajesService {
+
+    public static void crearMensaje(){
+
+    }
+
+    public static void listarMensaje(){
+
+    }
+
+    public static void borrarMensaje(){
+
+    }
+
+    public static void editarMensaje(){
+
+    }
+}
