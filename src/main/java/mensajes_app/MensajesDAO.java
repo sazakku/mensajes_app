7fc90adc1c53c562cba8af11ecfdccd2f2f6c2d7
Andres Galeano <afgaleano91@misena.edu.co>
2022-03-15 20:56:04 -0500

    create layer DAO for messages

---
+++ src/main/java/mensajes_app/MensajesDAO.java
@@ -0,0 +1,20 @@
+package mensajes_app;
+
+public class MensajesDAO {
+
+    public static void crearMensaje(){
+
+    }
+
+    public static void listarMensaje(){
+
+    }
+
+    public static void actualizarMensaje(){
+
+    }
+
+    public static void borrarMensaje(){
+
+    }
+}
